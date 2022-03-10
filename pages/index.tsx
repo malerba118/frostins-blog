70345a578ddc16bd1f5646aef0b385eeee3cc56d
Austin Malerba <austin.malerba@gmail.com>
2022-03-10 14:33:57 -0500

    rm dead code

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,21 +37,6 @@ const Home: NextPage = () => {
   return (
     <Project>
       <Box p={{ base: 4, md: 16 }}>
-        {/* <Heading lineHeight={2}>Reorder a list with framer-motion</Heading>
-        <Text>
-          Today we're going to learn how to reorder a list with framer-motion
-        </Text>
-        <Text>
-          First let's have a look at{" "}
-          <FileAction action="open" path="/index.js" />
-          as this is the root of our app.
-        </Text>
-        <br />
-        <Text>
-          Now let's add a new file <FileAction action="add" path="/foo.js" />.
-          Now that <InlineCode>/foo.js</InlineCode> exists, we can go ahead and
-          delete it <FileAction action="delete" path="/foo.js" />.
-        </Text> */}
         <ReorderList components={mdxComponents} />
       </Box>
     </Project>
